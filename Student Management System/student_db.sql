ced308ecb45c9890ae395ff9214ab58b33665733
Manuja Perera <manujaperera14@gmail.com>
2026-08-05 01:30:20 +0530

    Add files via upload

--- Student Management System/student_db.sql
+++ Student Management System/student_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 26, 2026 at 01:10 PM
+-- Generation Time: Aug 04, 2026 at 09:59 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,18 +30,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `students` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
+  `age` int(11) DEFAULT NULL,
   `course` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL
+  `email` varchar(100) NOT NULL,
+  `contact` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `name`, `course`, `email`) VALUES
-(1, 'Manuja Perera', 'Data Science', 'manujaperera2005@gmail.com'),
-(2, 'Kasun Liyanage', 'Software Engineer', 'kasunliyanage123@mail.com'),
-(3, 'Pubudu Perera', 'Computer Science', 'pubuduperera123@gmail.com');
+INSERT INTO `students` (`id`, `name`, `age`, `course`, `email`, `contact`) VALUES
+(1, 'Manuja Perera', 20, 'Data Science', 'manujaperera2005@gmail.com', '0752235897'),
+(2, 'Kasun Liyanage', NULL, 'Software Engineer', 'kasunliyanage123@mail.com', NULL),
+(3, 'Pubudu Perera', NULL, 'Computer Science', 'pubuduperera123@gmail.com', NULL);
 
 --
 -- Indexes for dumped tables
